cdf90833068dfe958b9ba89c996da600889813df
aaronpaul76 <aaronpaul76@gmail.com>
2017-03-22 12:24:40 +0530

    Update script.js

--- test/script.js
+++ test/script.js
@@ -37,3 +37,33 @@ document.addEventListener("DOMContentLoaded",
   
   }
 );
+
+function sum() 
+{ 
+ document.getElementById("add").checked = true;
+ document.getElementById("subtract").checked = false;
+ document.getElementById("multiply").checked = false;
+ document.getElementById("divide").checked = false;
+ var a, b, result, display; 
+ a = parseInt(document.getElementById("n1").value, 10);
+ b = parseInt(document.getElementById("n2").value, 10);
+ result = (a+b);
+ display = "<h4> The result is: " + result + "</h4>"; 
+
+ document.getElementById("demo2").innerHTML = display; 
+}
+
+function diff() 
+{ 
+ document.getElementById("subtract").checked = true;
+ document.getElementById("add").checked = false;
+ document.getElementById("multiply").checked = false;
+ document.getElementById("divide").checked = false;
+ var a, b, result, display; 
+ a = parseInt(document.getElementById("n1").value, 10);
+ b = parseInt(document.getElementById("n2").value, 10);
+ result = (a-b); 
+ display = "<h4> The result is: " + result + "</h4>"; 
+
+ document.getElementById("demo2").innerHTML = display; 
+}
